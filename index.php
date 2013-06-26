f5e2338f354deb9601e144f97769818e2b9fae8d
Adam Kalsey <adam@kalsey.com>
2013-06-26 14:16:07 -0700

    (New) Individual URL buckets. Get different URLs for testing different requests.

--- index.php
+++ index.php
@@ -1,35 +1,50 @@
 <?php
 include 'compat.php';
 
-$files = glob("*.txt");
+$files = glob("log/*.txt");
 rsort($files);
 foreach ($files as $filename) {
   $timestamp = str_replace('.txt', '', $filename);
+  $timestamp = substr($timestamp, 18);
   if ($timestamp < time() - 60*60*24) {
     unlink($filename);
   }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  print "<h1>Poster</h1>";
-  foreach ($files as $filename) {
-    $timestamp = str_replace('.txt', '', $filename);
-    echo "<h4>" . date('F j, Y g:i:s e',$timestamp) . "</h4\n";
-    $file = fopen($filename, "r");
-    $contents = fread($file, filesize($filename));
-    fclose($file);
-    echo "<pre>\n" . htmlentities($contents) . "\n</pre>\n<hr/>\n";
+  if (array_key_exists('PATH_INFO', $_SERVER)) {
+    print "<h1>Poster</h1>";
+    print "<p>Post to <code>http://{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}</code></p>";
+    foreach ($files as $filename) {
+      $id = substr($_SERVER['PATH_INFO'], 1);
+      if (stripos($filename, $id) == 4) {
+        $timestamp = str_replace('.txt', '', $filename);
+        $timestamp = str_replace('log/'.$id . '-', '', $timestamp);
+        $file = fopen($filename, "r");
+        $contents = fread($file, filesize($filename));
+        fclose($file);
+        echo "\n<hr/><pre>\n" . htmlentities($contents) . "\n</pre>\n";        
+      }
+    }
+  } else {
+    $path = uniqid();
+    header('Location: ' . $_SERVER['PHP_SELF'] . '/' . $path); 
   }
+
 } else {
-  $req = "{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']}\n";
-  $req .= "?{$_SERVER['QUERY_STRING']}\n";
+  $req = date('r') . "\n";
+  $req .= "{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']}\n";
+  if (array_key_exists('QUERY_STRING', $_SERVER)) {
+    $req .= "?{$_SERVER['QUERY_STRING']}\n";
+  }
   
   $headers = apache_request_headers();
   foreach ($headers as $header => $value) {
       $req .= "$header: $value\n";
   }
   $req .= "\n". file_get_contents("php://input");
-  $file = fopen(time(). ".txt", 'w');
+  $filename = dirname(__FILE__) . '/log' . $_SERVER['PATH_INFO'] . '-' . time(). ".txt";
+  $file = fopen($filename, 'w');
   fwrite($file, $req);
   fclose($file);
 }
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
 Poster
 ======
 
-Debugging web services applications and discovering what the POST and PUT requests look like is sometimes difficult. Poster gives you a single URL that you can point web services at. POST, PUT, DELETE, and HEAD requsts will write the request details (including http headers) to a log. GETs to the url (like from your browser) will display the log. 
+Debugging web services applications and discovering what the POST and PUT requests look like is sometimes difficult. Poster gives you a single URL that you can point web services at. POST, PUT, DELETE, and HEAD requests will write the request details (including http headers) to a log. GETs to the url (like from your browser) will display the log. 
 
 The log rolls over every 24 hours. Any items older than 24 hours will be deleted.
 
 Installation
 ------------
 
-Copy the poster directory to your web server. Make sure the directory is writable by the web server, as log files will be writen to it.
+Copy the poster directory to your web server. Create a "log" directory inside this directory and make sure the log directory is writable by the web server, as log files will be writen to it.
